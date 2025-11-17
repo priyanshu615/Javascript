45e285660bbab652e0152e964329f5237e121515
Priyanshu Mehta <priyanshumehta294@gmail.com>
2025-11-18 01:49:19 +0530

    update

---
+++ 01_basic/03_ConversionOperation.js
@@ -0,0 +1,28 @@
+let score = "33"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)  //Number is used to convert into number
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN //not a number
+//  true => 1; false => 0
+
+let isLoggedIn = 0
+
+let booleanIsLoggedIn = Boolean(isLoggedIn) // used to convert into boolean
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "priyanshu" => true
+
+let someNumber = 45
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber);
