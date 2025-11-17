089f307c57a9f454038240e90b2d06c1f0a1c249
Priyanshu Mehta <priyanshumehta294@gmail.com>
2025-11-18 02:01:23 +0530

    update

--- 01_basic/03_ConversionOperation.js
+++ 01_basic/03_ConversionOperation.js
@@ -1,11 +1,11 @@
 let score = "33"
 
-// console.log(typeof score);
-// console.log(typeof (score));
+console.log(typeof score);
+console.log(typeof (score));
 
-// let valueInNumber = Number(score)  //Number is used to convert into number
-// console.log(typeof valueInNumber);
-// console.log(valueInNumber);
+let valueInNumber = Number(score)  //Number is used to convert into number
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
 
 
 // "33" => 33
@@ -26,3 +26,23 @@ let someNumber = 45
 let stringNumber = String(someNumber)
 console.log(stringNumber)
 console.log(typeof stringNumber);
+
+// ********** Operations ******
+
+let value = 4
+let negValue = -value
+
+//console.log(negValue)
+
+// console.log(2+5)
+// console.log(4-4)
+// console.log(2*6)
+// console.log(2**5)
+
+let str1 = "Priyanshu"
+let str2 = "Mehta"
+
+console.log(str1 + " " + str2)
+
+
+
