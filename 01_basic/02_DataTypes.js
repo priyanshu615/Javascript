c40f2805abdd8197584e328dc6c2bda33e6828be
Priyanshu Mehta <priyanshumehta294@gmail.com>
2025-11-17 23:24:28 +0530

    dataTypes

---
+++ 01_basic/02_DataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; //treat JS code as new version
+
+//alert(3+4) //we are using nodejs, not browser
+console.log(3+3);
+
+let name = "Priyanshu Mehta"
+let age = "20"
+let isLoggedIn = false
+let state; //returns undefined
+
+
+
+// number => 2 to power 53
+// bigint => bigger value of int
+// string => " "
+// boolean => true/false
+// null => standalone value 
+// undefined => jo value bss declared hota..jispe koi value nahi
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
