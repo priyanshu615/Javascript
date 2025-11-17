4d736f5dde890d7588bd6cbebf9a81ff8d0fdeaf
Priyanshu Mehta <priyanshumehta294@gmail.com>
2025-11-18 02:14:22 +0530

    update

---
+++ 01_Basics/04_comparison.js
@@ -0,0 +1,14 @@
+// console.log(2==2)
+// console.log(2>1)
+// console.log(2<1)
+console.log("2" > 1)
+
+
+// console.log(null > 0)
+// console.log(null == 0)
+// console.log(null >= 0)  // Avoid this type of comparision of null and undefined..this is a kind of bad practice while writing code
+
+// console.log(undefined == 0)
+
+
+
