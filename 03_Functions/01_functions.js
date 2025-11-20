2b9c9ac1ff8374def9538e6d4c55c672f8ab8b9d
Priyanshu Mehta <priyanshumehta294@gmail.com>
2025-11-21 02:20:53 +0530

    update

---
+++ 03_Functions/01_functions.js
@@ -0,0 +1,24 @@
+
+function sayMyName(){
+    console.log("N");
+    console.log("I");
+    console.log("T");
+    console.log("I");
+    console.log("H")
+
+}
+
+// sayMyName        //writing just name is for the reference
+// sayMyName()            //writing name with (), which calls the function
+
+function addTwoNumbers(number1, number2){
+
+    console.log(number1 + number2);
+}
+
+addTwoNumbers(3,5)
+
+function add(n1, n2){
+    return n1 + n2
+    //return ke badd..function ka koi bhi statemnt excute nahi hoga..isliye kch bhi agar statement add krna hai toh return ke phle add krna pdega 
+}
